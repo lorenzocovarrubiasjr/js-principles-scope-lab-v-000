02498e51a58b4a8e20117951a99efd5948856eb6
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-01-04 02:29:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+   return bestCustomer;
+}
+
+function overwriteBestCustomer(a) {
+  bestCustomer = a;
+  return bestCustomer;
+}
+
+  const leastFavoriteCustomer = "not me";
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "def not me";
+  return leastFavoriteCustomer;
+}
